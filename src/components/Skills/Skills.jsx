5dc6ab613c13ac9cb24031fa334120557bff3a96
ihza6661 <ihzahmahendra6661@gmail.com>
2025-11-22 17:05:20 +0700

    remove basic skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,8 @@
-import styles from './SkillsStyles.module.scss';
-import checkMarkIcon from '../../assets/checkmark-dark.svg';
+import styles from "./SkillsStyles.module.scss";
+import checkMarkIcon from "../../assets/checkmark-dark.svg";
 // import checkMarkIconLight from '../../assets/checkmark-light.svg';
-import SkillList from '../../common/SkillList';
+import SkillList from "../../common/SkillList";
 // import { useTheme } from '../../common/ThemeContext';
-
 
 function Skills() {
   // const { theme } = useTheme();
@@ -11,36 +10,25 @@ function Skills() {
 
   return (
     <section id="skills" className={styles.container}>
-      <h1 className="sectionTitle">
-Technical Skills
-</h1>
+      <h1 className="sectionTitle">Technical Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
-        {/* <SkillList src={checkMarkIcon} skill="TypeScript" /> */}
-        {/* <SkillList src={checkMarkIcon} skill="Node" /> */}
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="Laravel" />
-        {/* <SkillList src={checkMarkIcon} skill="Vue" /> */}
         {/* <SkillList src={checkMarkIcon} skill="Tailwind CSS" /> */}
       </div>
       <hr />
       <div className={styles.skillList}>
-        {/* <SkillList src={checkMarkIcon} skill="Redux" /> */}
-        {/* <SkillList src={checkMarkIcon} skill="Webpack" /> */}
         <SkillList src={checkMarkIcon} skill="Git" />
-        {/* <SkillList src={checkMarkIcon} skill="Jest" /> */}
         <SkillList src={checkMarkIcon} skill="Sql" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Python" />
-
       </div>
     </section>
   );
