ad86a15e2876b97795653357093a772d5d910eb2
ihza6661 <ihzahmahendra6661@gmail.com>
2025-12-12 15:53:15 +0700

    des

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,13 +1,8 @@
 import styles from "./SkillsStyles.module.scss";
 import checkMarkIcon from "../../assets/checkmark-dark.svg";
-// import checkMarkIconLight from '../../assets/checkmark-light.svg';
 import SkillList from "../../common/SkillList";
-// import { useTheme } from '../../common/ThemeContext';
 
 function Skills() {
-  // const { theme } = useTheme();
-  // const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
-
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Technical Skills</h1>
