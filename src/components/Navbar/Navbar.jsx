9434d2a36dc3a31b81ad6abbef09fada60c2097e
ihza6661 <ihzahmahendra6661@gmail.com>
2025-12-12 18:11:56 +0700

    remove linkin

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import Sidebar from "../Sidebar/Sidebar";
 
 const Navbar = () => {
-
   return (
     <div className="navbar">
       <Sidebar />
@@ -12,9 +11,7 @@ const Navbar = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-        >
-
-        </motion.span>
+        ></motion.span>
         <div className="social">
           {/* <a href="https://www.facebook.com/Casablancas6661/" target="blank"> */}
           {/*   <img src="/facebook.png" alt="facebook icon" /> */}
@@ -22,9 +19,9 @@ const Navbar = () => {
           <a href="https://github.com/ihza6661/" target="blank">
             <img src="/github_dark.png" alt="github icon" />
           </a>
-          <a href="https://github.com/ihza6661/" target="blank">
-            <img src="/linkedin_dark.png" alt="linkedin icon" />
-          </a>
+          {/* <a href="https://github.com/ihza6661/" target="blank"> */}
+          {/*   <img src="/linkedin_dark.png" alt="linkedin icon" /> */}
+          {/* </a> */}
           <a href="https://www.instagram.com/ihza.ms/" target="blank">
             <img src="/instagram.png" alt="instagram icon" />
           </a>
